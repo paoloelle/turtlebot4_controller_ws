c452abe2e2ff245cb3d814e2304df9577bff3c96
paoloelle <paolo.leopardi@uni-konstanz.de>
2024-02-02 16:28:13 +0100

    improved scan filtering

--- turtlebot4_controller/turtlebot4_controller_node.py
+++ turtlebot4_controller/turtlebot4_controller_node.py
@@ -28,9 +28,12 @@ class Controller_Node(Node):
             qos_profile_sensor_data
         )
 
-        self.bumper = 0
 
-        self.filtered_scan = None 
+        self.filtered_scan = []
+        
+        self.section_limits = [(-pi/8, pi/8), (pi/8, 3/8*pi), (3/8*pi, 5/8*pi),
+                               (5/8*pi, 7/8*pi), (-pi/8, -3/8*pi), (-3/8*pi, -5/8*pi),
+                               (-5/8*pi, -7/8*pi), (-7/8*pi, 7/8*pi)]
 
         #self.ann_controller = controller(input_size, hidden_size, output_size)
 
@@ -43,24 +46,53 @@ class Controller_Node(Node):
 
 
     def scan_callback(self, scan_msg):
+
+        # split laser scan lectures in 8 sections as follows:
+        # [-pi/8, pi/8], [pi/8, 3/8 pi], [3/8 pi, 5/8 pi], [5/8 pi, 7/8 pi]
+        # [-pi/8, -3/8 pi], [-3/8 pi, -5/8 pi], [-5/8 pi, -7/8 pi], [-7/8 pi, 7/8 pi]
+
+        switch_lectures = True  
         
-        self.get_min_distance(self, scan_msg)
+        for angle_limits in self.section_limits:
+            min_angle_lim, max_angle_lim = angle_limits
 
+            min_index, max_index = self.get_reference_index(scan_msg, min_angle_lim, max_angle_lim)  
+
+            if angle_limits[-1][0] == min_angle_lim and angle_limits[-1][1] == max_angle_lim:
+                switch_lectures = True
+
+            min_distance = self.get_min_distance(scan_msg, min_index, max_index, switch_lectures)
+            
+            self.filtered_scan.append(min_distance)
+            
+        print(self.fiiltred_scan)
+        
+        
 
 
         
-    def get_min_distance(self, scan_msg):
+    def get_min_distance(self, scan_msg, min_index, max_index, switch_lectures):
 
-        min_index, max_index = self.get_reference_index(scan_msg, pi/8, pi)
-        print(min_index)
-        print(max_index)
+        
+
+        if switch_lectures:
+            section_lectures = [scan_msg.ranges[0:min_index], scan_msg.ranges[max_index, 0]]
+        else: 
+            section_lectures = scan_msg.ranges[min_index:max_index]
+
+
+        # normalise lectures between 0 and 1
+        normalized_lectures = [(x - scan_msg.range_min) / (scan_msg.range_max - scan_msg.range_min) for x in section_lectures]
+        normalized_lectures = [scan_msg.range_min if x==float('inf') else x for x in normalized_lectures] # remove inf values
+
+        return min(normalized_lectures)
 
         
         
 
-        #TODO normalize ranges between 0 and 1
 
-    def get_reference_index(self, scan_msg, min_angle, max_angle):
+    @staticmethod
+    def get_reference_index(scan_msg, min_angle, max_angle):
                   
         min_angle = min_angle + pi
         max_angle = max_angle + pi
@@ -71,8 +103,8 @@ class Controller_Node(Node):
         lower_index = int((min_angle - msg_angle_min) /scan_msg.angle_increment)
         upper_index = int((max_angle - msg_angle_min) /scan_msg.angle_increment)
 
-        lower_index = 1
-        upper_index = 2
+        if lower_index > upper_index:
+            lower_index, upper_index = upper_index, lower_index
 
         return lower_index, upper_index
 
