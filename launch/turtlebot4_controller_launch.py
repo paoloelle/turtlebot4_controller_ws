f20ef93e3e13b41cf3264f38dde1c257c157e9f1
paoloelle <paolo.leopardi@uni-konstanz.de>
2024-02-06 15:11:33 +0100

    added launch file

--- launch/turtlebot4_controller_launch.py
+++ launch/turtlebot4_controller_launch.py
@@ -1,12 +1,29 @@
 import launch
 import launch_ros.actions
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
 
 # TODO
 # launch file for the gazebo simulation
 # launch the node to control the turtlebot
-# see how to spawn multiple objects and save the number of object stored and remove the object from the nest when stored
+# save the number of object stored 
 
 def generate_launch_description():
-    return LaunchDescription([
+    return launch.LaunchDescription([
+
+        launch_ros.actions.Node(
+            package = 'turtlebot4_controller',
+            executable='turtlebot4_controller_node',
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                FindPackageShare("turtlebot4_ignition_bringup"), '/launch', '/turtlebot4_ignition.launch.py'
+            ])      
+
+        )
+
+        
         
     ])
