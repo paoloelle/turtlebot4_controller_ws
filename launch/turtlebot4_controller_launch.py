cdfc12e014639ebcc530544a71852b114003992a
paoloelle <paolo.leopardi@uni-konstanz.de>
2024-02-06 15:50:42 +0100

    launch file

--- launch/turtlebot4_controller_launch.py
+++ launch/turtlebot4_controller_launch.py
@@ -1,12 +1,13 @@
 import launch
 import launch_ros.actions
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
 
 # TODO
 # launch file for the gazebo simulation
-# launch the node to control the turtlebot
+# launch the node to control the turtlebot with the weights from the neuroevolution
 # save the number of object stored 
 
 def generate_launch_description():
@@ -17,10 +18,25 @@ def generate_launch_description():
             executable='turtlebot4_controller_node',
         ),
 
+        DeclareLaunchArgument(
+            'world',
+            default_value='arena'
+        ),
+
+        DeclareLaunchArgument(
+            'z',
+            default_value='0.2'
+        ),
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
                 FindPackageShare("turtlebot4_ignition_bringup"), '/launch', '/turtlebot4_ignition.launch.py'
-            ])      
+            ]),
+
+        launch_arguments={
+            'world':LaunchConfiguration('world'),
+            'z':LaunchConfiguration('z')
+        }.items()
 
         )
 
