ec09eba911cf1d0f55a713c0db503707164c14da
paoloelle <paolo.leopardi@uni-konstanz.de>
2024-02-07 17:34:15 +0100

    found how to pass weights parameter to controller

--- launch/turtlebot4_controller_launch.py
+++ launch/turtlebot4_controller_launch.py
@@ -2,21 +2,28 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+
 def generate_launch_description():
-    config = os.path.join(
-        get_package_share_directory('turtlebot4_controller'),
-        'config',
-        'weights.yaml'
+
+    weights_arg = DeclareLaunchArgument(
+        'weights', default_value=TextSubstitution(text='[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]')
     )
 
+
     return LaunchDescription([
+        weights_arg,
         Node(
             package='turtlebot4_controller',
             executable='turtlebot4_controller_node',
-            name='ANN_controller',
-            parameters=[config]            
+            name='ann_controller',
+            parameters=[{
+                'weights':LaunchConfiguration('weights')
+            }]
         )
     ])
