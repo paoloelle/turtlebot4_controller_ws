2884cab17eb4f5ca1d18c74b10d87eb7c854ce19
Paolo Leopardi <paolo.leopardi@uni-konstanz.de>
2024-04-08 16:17:01 +0200

    added 3 lights tf

--- launch/tf_broadcaster_launch.py
+++ launch/tf_broadcaster_launch.py
@@ -6,6 +6,17 @@ def generate_launch_description():
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
-            arguments = ['--x', '0', '--y', '0', '--z', '1', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'base_link', '--child-frame-id', 'light_sensor']
+            arguments = ['--x', '0.09', '--y', '0.08', '--z', '0.35', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'base_link', '--child-frame-id', 'light_sensor_frontL']
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments = ['--x', '0.09', '--y', '-0.08', '--z', '0.35', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'base_link', '--child-frame-id', 'light_sensor_frontR']
+        ),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments = ['--x', '-0.11', '--y', '0', '--z', '0.35', '--yaw', '0', '--pitch', '0', '--roll', '0', '--frame-id', 'base_link', '--child-frame-id', 'light_sensor_back']
         ),
     ])
